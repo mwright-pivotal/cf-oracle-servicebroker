755e137f81c9122d96d0ee44410e7cf788e37046
Mike Wright <mwright@pivotal.io>
2017-08-09 14:04:20 -0500

    fix typo

--- src/main/java/com/pivotal/cf/broker/model/ServiceDefinition.java
+++ src/main/java/com/pivotal/cf/broker/model/ServiceDefinition.java
@@ -55,7 +55,7 @@ public class ServiceDefinition {
 	
 	@JsonSerialize
 	@JsonProperty("plans")
-	@OneToMany(fetch=FetchType.LAZY:)
+	@OneToMany(fetch=FetchType.LAZY)
 	@JoinColumn(name="service_definition_id")
 	private List<Plan> plans = new ArrayList<Plan>();
 	
